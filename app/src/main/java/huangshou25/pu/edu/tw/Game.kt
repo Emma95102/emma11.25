cc94af3a7cf15a8ad01f4e0729457dc0eda28e43
student <zilingh16@gmail.com>
2024-11-25 16:41:38 +0800

    1次

--- app/src/main/java/huangshou25/pu/edu/tw/Game.kt
+++ app/src/main/java/huangshou25/pu/edu/tw/Game.kt
@@ -1,24 +1,80 @@
 package huangshou25.pu.edu.tw
+import android.content.Context
+import android.media.MediaPlayer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlin.math.abs
-class Game(val scope: CoroutineScope, val screenW:Int, val screenH: Int) {
+class Game(val scope: CoroutineScope, screenW: Int, screenH: Int,scale:Float, context: Context) {
+//背景音樂
+
+    var mper1 = MediaPlayer.create(context, R.raw.lastletter)
+
+//結束音樂
+
+    var mper2 = MediaPlayer.create(context, R.raw.gameover)
     var counter = 0
+
     val state = MutableStateFlow(0)
+
     val background = Background(screenW)
+
+    val boy = Boy(screenH, scale)
+    val virus = Virus(screenW, screenH, scale)
+
     var isPlaying = true
-    fun Play() {
+
+    fun Play(){
+
         scope.launch {
+
 //counter = 0
+
             isPlaying = true
+
             while (isPlaying) {
-                delay(10)
+                mper1.start() //播放背景音樂
+
+                delay(40)
+
                 background.Roll()
+
+                if (counter % 3 == 0){
+
+                    boy.Walk()
+                    virus.Fly()
+                    //判斷是否碰撞，結束遊戲
+
+                    if(boy.getRect().intersect(virus.getRect())) {
+
+                        isPlaying = false
+                        //遊戲結束音效
+
+                        mper1.pause()
+
+                        mper2.start()
+
+                    }
+
+                }
+
                 counter++
+
                 state.emit(counter)
+
             }
         }
+    }
+    fun Restart(){
+
+        virus.Reset()
+
+        counter = 0
+
+        isPlaying = true
+
+        Play()
+
     }
 }
